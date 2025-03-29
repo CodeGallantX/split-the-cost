e54bc7a1535363049d4071b20c20917d8cd30fcb
John A. Samuel <johnayobami77@proton.me>
2025-03-29 23:32:30 +0100

    added forgot-password page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {Routes, Route} from "react-router-dom"
 import Login from './pages/auth/login';
 import SignUp from './pages/auth/signup';
 import Verification from './pages/auth/verification';
-// import ForgotPassword from './pages/auth/forgot-password';
+import ForgotPassword from './pages/auth/forgot-password';
 import ResetPassword from './pages/auth/reset-password';
 // import Dashboard from './pages/dashboard';
 import './index.css';
@@ -15,7 +15,7 @@ function App() {
       <Route path="/auth/login" element={<Login />} />
       <Route path="/auth/signup" element={<SignUp />} />
       <Route path="/auth/verification" element={<Verification />} />
-      {/* <Route path="/auth/forgot-password" element={<ForgotPassword />} /> */}
+      <Route path="/auth/forgot-password" element={<ForgotPassword />} />
       <Route path="/auth/reset-password" element={<ResetPassword />} />
       
       {/* Logged in */}
